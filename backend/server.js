93207fe50df8edbb5a4f67059dddc445b2fcd6f5
huuthang28cb <huuthang28.work@gmail.com>
2022-02-18 17:00:29 +0700

    Error Middleware and Routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
-import products from "./data/Products.js";
 import dotenv from "dotenv";
 import connectDatabase from "./config/MongoDb.js";
+import ImportData from "./DataImport.js";
+import productRoute from "./Routes/ProductRoutes.js";
+import { errorHandler, notFound } from "./Middleware/Errors.js";
 
 dotenv.config();
 const app = express();
@@ -9,18 +11,14 @@ const app = express();
 // connect database
 connectDatabase();
 
-// Load product from Server
-app.get("/api/products", (req, res) => {
-    res.json(products)
-})
-
-// Single product from Server
-app.get("/api/products/:id", (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-})
+// API
+app.use("/api/import", ImportData);
+app.use("/api/products", productRoute);
 
 
+// ERROR HANDLER
+app.use(notFound);
+app.use(errorHandler);
 
 app.get("/", (req, res) => {
     res.send("API is running...");
