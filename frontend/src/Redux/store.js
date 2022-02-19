a0b477ebe9ac0a263416883105a4576be5d2fb6b
huuthang28cb <huuthang28.work@gmail.com>
2022-02-19 16:28:57 +0700

    Add to cart redux

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -1,19 +1,29 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import { 
-    productDetailsReducer, 
-    productListReducer 
+import {
+    productDetailsReducer,
+    productListReducer
 } from "./Reducers/ProductReducers";
+import { cartReducer } from "./Reducers/CartReducers";
 
 const reducer = combineReducers({
     productList: productListReducer,
-    productDetails: productDetailsReducer
+    productDetails: productDetailsReducer,
+    cart: cartReducer
 });
 
 const middleware = [thunk];
 
-const initialState = {};
+const cartItemsFromLocalStorage = localStorage.getItem("cartItems")
+    ? JSON.parse(localStorage.getItem("cartItems"))
+    : [];
+
+const initialState = {
+    cart: {
+        cartItems: cartItemsFromLocalStorage,
+    }
+};
 
 const store = createStore(
     reducer,
--- frontend/src/screens/SingleProduct.js
+++ frontend/src/screens/SingleProduct.js
@@ -1,4 +1,4 @@
-import React, { useEffect} from "react";
+import React, { useEffect, useState } from "react";
 import Header from "../Components/Header";
 import Rating from "../Components/homeComponents/Rating";
 import { Link } from "react-router-dom";
@@ -9,7 +9,10 @@ import { listProductDetails } from "../Redux/Actions/ProductActions";
 import Loading from "../Components/LoadingError/Loading";
 
 
-const SingleProduct = ({ match }) => {
+const SingleProduct = ({ history, match }) => {
+
+    const [qty, setQty] = useState(1);
+
     const productID = match.params.id;
 
     const dispatch = useDispatch();
@@ -22,6 +25,12 @@ const SingleProduct = ({ match }) => {
         dispatch(listProductDetails(productID));
     }, [dispatch, productID])
 
+    // Add to cart handled
+    const AddToCartHandle = (e) => {
+        e.preventDefault()
+        history.push(`/cart/${productID}?qty=${qty}`);
+    }
+
     return (
         <>
             <Header />
@@ -30,126 +39,126 @@ const SingleProduct = ({ match }) => {
                     loading ? (
                         <Loading />
                     )
-                    : error ? (
-                        <Message variant="alert-danger">
-                            {error}
-                        </Message>
-                    )
-                    :
-                    (
-                        <>
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <div className="single-image">
-                                        <img src={product.image} alt={product.name} />
-                                    </div>
-                                </div>
-                                <div className="col-md-6">
-                                    <div className="product-dtl">
-                                        <div className="product-info">
-                                            <div className="product-name">{product.name}</div>
+                        : error ? (
+                            <Message variant="alert-danger">
+                                {error}
+                            </Message>
+                        )
+                            :
+                            (
+                                <>
+                                    <div className="row">
+                                        <div className="col-md-6">
+                                            <div className="single-image">
+                                                <img src={product.image} alt={product.name} />
+                                            </div>
                                         </div>
-                                        <p>{product.description}</p>
+                                        <div className="col-md-6">
+                                            <div className="product-dtl">
+                                                <div className="product-info">
+                                                    <div className="product-name">{product.name}</div>
+                                                </div>
+                                                <p>{product.description}</p>
 
-                                        <div className="product-count col-lg-7 ">
-                                            <div className="flex-box d-flex justify-content-between align-items-center">
-                                                <h6>Giá</h6>
-                                                <span>${product.price}</span>
-                                            </div>
-                                            <div className="flex-box d-flex justify-content-between align-items-center">
-                                                <h6>Trạng thái hàng</h6>
-                                                {product.countInStock > 0 ? (
-                                                    <span className='text-success'>Còn hàng</span>
-                                                ) : (
-                                                    <span className='text-danger'>Hết hàng</span>
-                                                )}
-                                            </div>
-                                            <div className="flex-box d-flex justify-content-between align-items-center">
-                                                <h6>Đánh giá</h6>
-                                                <Rating
-                                                    value={product.rating}
-                                                    text={`${product.numReviews} đánh giá`}
-                                                />
-                                            </div>
-                                            {product.countInStock > 0 ? (
-                                                <>
+                                                <div className="product-count col-lg-7 ">
                                                     <div className="flex-box d-flex justify-content-between align-items-center">
-                                                        <h6>Số lượng</h6>
-                                                        <select>
-                                                            {[...Array(product.countInStock).keys()].map((x) => (
-                                                                <option key={x + 1} value={x + 1}>
-                                                                    {x + 1}
-                                                                </option>
-                                                            ))}
-                                                        </select>
+                                                        <h6>Giá</h6>
+                                                        <span>${product.price}</span>
                                                     </div>
-                                                    <button className="round-black-btn">Thêm vào giỏ hàng</button>
-                                                </>
-                                            ) : null}
+                                                    <div className="flex-box d-flex justify-content-between align-items-center">
+                                                        <h6>Trạng thái hàng</h6>
+                                                        {product.countInStock > 0 ? (
+                                                            <span className='text-success'>Còn hàng</span>
+                                                        ) : (
+                                                            <span className='text-danger'>Hết hàng</span>
+                                                        )}
+                                                    </div>
+                                                    <div className="flex-box d-flex justify-content-between align-items-center">
+                                                        <h6>Đánh giá</h6>
+                                                        <Rating
+                                                            value={product.rating}
+                                                            text={`${product.numReviews} đánh giá`}
+                                                        />
+                                                    </div>
+                                                    {product.countInStock > 0 ? (
+                                                        <>
+                                                            <div className="flex-box d-flex justify-content-between align-items-center">
+                                                                <h6>Số lượng</h6>
+                                                                <select>
+                                                                    {[...Array(product.countInStock).keys()].map((x) => (
+                                                                        <option key={x + 1} value={x + 1}>
+                                                                            {x + 1}
+                                                                        </option>
+                                                                    ))}
+                                                                </select>
+                                                            </div>
+                                                            <button onClick={AddToCartHandle} className="round-black-btn">Thêm vào giỏ hàng</button>
+                                                        </>
+                                                    ) : null}
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
 
-                            {/* RATING */}
-                            <div className="row my-5">
-                                <div className="col-md-6">
-                                    <h6 className="mb-3">Đánh giá và nhận xét</h6>
-                                    <Message variant={"alert-info mt-3"}>Không có nhận xét</Message>
-                                    <div className="mb-5 mb-md-3 bg-light p-3 shadow-sm rounded">
-                                        <strong>Admin</strong>
-                                        <Rating />
-                                        <span>Jan 12 2021</span>
-                                        <div className="alert alert-info mt-3">
-                                            Lorem Ipsum is simply dummy text of the printing and typesetting
-                                            industry. Lorem Ipsum has been the industry's standard dummy
-                                            text ever since the 1500s, when an unknown printer took a galley
-                                            of type and scrambled it to make a type specimen book
+                                    {/* RATING */}
+                                    <div className="row my-5">
+                                        <div className="col-md-6">
+                                            <h6 className="mb-3">Đánh giá và nhận xét</h6>
+                                            <Message variant={"alert-info mt-3"}>Không có nhận xét</Message>
+                                            <div className="mb-5 mb-md-3 bg-light p-3 shadow-sm rounded">
+                                                <strong>Admin</strong>
+                                                <Rating />
+                                                <span>Jan 12 2021</span>
+                                                <div className="alert alert-info mt-3">
+                                                    Lorem Ipsum is simply dummy text of the printing and typesetting
+                                                    industry. Lorem Ipsum has been the industry's standard dummy
+                                                    text ever since the 1500s, when an unknown printer took a galley
+                                                    of type and scrambled it to make a type specimen book
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>
-                                <div className="col-md-6">
-                                    <h6>BÀI VIẾT ĐÁNH GIÁ CỦA KHÁCH HÀNG</h6>
-                                    <div className="my-4"></div>
+                                        <div className="col-md-6">
+                                            <h6>BÀI VIẾT ĐÁNH GIÁ CỦA KHÁCH HÀNG</h6>
+                                            <div className="my-4"></div>
 
-                                    <form>
-                                        <div className="my-4">
-                                            <strong>Xếp hạng</strong>
-                                            <select className="col-12 bg-light p-3 mt-2 border-0 rounded">
-                                                <option value="">Chọn...</option>
-                                                <option value="1">1 - Quá tệ</option>
-                                                <option value="2">2 - Bình thường</option>
-                                                <option value="3">3 - Tốt</option>
-                                                <option value="4">4 - Rất tốt</option>
-                                                <option value="5">5 - Quá tuyệt vời</option>
-                                            </select>
+                                            <form>
+                                                <div className="my-4">
+                                                    <strong>Xếp hạng</strong>
+                                                    <select className="col-12 bg-light p-3 mt-2 border-0 rounded">
+                                                        <option value="">Chọn...</option>
+                                                        <option value="1">1 - Quá tệ</option>
+                                                        <option value="2">2 - Bình thường</option>
+                                                        <option value="3">3 - Tốt</option>
+                                                        <option value="4">4 - Rất tốt</option>
+                                                        <option value="5">5 - Quá tuyệt vời</option>
+                                                    </select>
+                                                </div>
+                                                <div className="my-4">
+                                                    <strong>Nhận xét</strong>
+                                                    <textarea
+                                                        row="3"
+                                                        className="col-12 bg-light p-3 mt-2 border-0 rounded"
+                                                    ></textarea>
+                                                </div>
+                                                <div className="my-3">
+                                                    <button className="col-12 bg-black border-0 p-3 rounded text-white">
+                                                        Gửi nhận xét của bạn
+                                                    </button>
+                                                </div>
+                                            </form>
+                                            <div className="my-3">
+                                                <Message variant={"alert-warning"}>
+                                                    Vui lòng{" "}
+                                                    <Link to="/login">
+                                                        " <strong>Đăng nhập</strong> "
+                                                    </Link>{" "}
+                                                    để viết đánh giá sản phẩm của bạn{" "}
+                                                </Message>
+                                            </div>
                                         </div>
-                                        <div className="my-4">
-                                            <strong>Nhận xét</strong>
-                                            <textarea
-                                                row="3"
-                                                className="col-12 bg-light p-3 mt-2 border-0 rounded"
-                                            ></textarea>
-                                        </div>
-                                        <div className="my-3">
-                                            <button className="col-12 bg-black border-0 p-3 rounded text-white">
-                                                Gửi nhận xét của bạn
-                                            </button>
-                                        </div>
-                                    </form>
-                                    <div className="my-3">
-                                        <Message variant={"alert-warning"}>
-                                            Vui lòng{" "}
-                                            <Link to="/login">
-                                                " <strong>Đăng nhập</strong> "
-                                            </Link>{" "}
-                                            để viết đánh giá sản phẩm của bạn{" "}
-                                        </Message>
                                     </div>
-                                </div>
-                            </div>
-                        </>
-                    )
+                                </>
+                            )
                 }
             </div>
             <ContactInfo />
